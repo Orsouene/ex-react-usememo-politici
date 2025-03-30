676ed88cfb6079634bf311544c53ea11cfa86841
Orsouene <orsoueneelaouizeb@gmail.com>
2025-03-30 23:42:29 +0200

    Milestone 2

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,9 +4,9 @@ function Card({ politico }) {
     return (
         <div className='bg-stone-200 mb-3 text-stone-700 rounded-xl max-w-64 p-2 m-auto h-56'>
             <h3><span className='text-xl text-black'>Nome :</span>{politico.name}</h3>
-            <div> <span className='text-xl text-black'>Immagine :</span>
+            {/* <div> <span className='text-xl text-black'>Immagine :</span>
                 <img src="http://someImage.png" alt="" />
-            </div>
+            </div> */}
 
             <p> <span className='text-xl text-black'>Posizione :</span>{politico.occupation}</p>
             <p > <span className='text-xl text-black'>Breve biografia  :</span>my name is {politico.name} and my hobbies are {politico.hobbies[0]} and {politico.hobbies[1]} </p>
--- src/Components/Politici.jsx
+++ src/Components/Politici.jsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useMemo, useState } from 'react'
 import Card from './Card';
 function Politici() {
     const [PoliticiState, setPoliticiState] = useState([])
-
+    const [filtro, setFiltro] = useState("")
     const dataFetch = () => {
 
         fetch("https://boolean-spec-frontend.vercel.app/freetestapi/users").then(res => res.json()).then(data => {
@@ -16,21 +16,42 @@ function Politici() {
         dataFetch();
     }, []);
 
+    function handleInput(e) {
+        const value = e.target.value
+        setFiltro(value)
+    }
+
+    const filtredArray = useMemo(() => {
+        if (!filtro) {
+            return PoliticiState
+        }
+        else {
+            return PoliticiState.filter(politico => { return politico.name.toLowerCase().includes(filtro.toLowerCase()) || politico.hobbies.find((el) => el.toLowerCase().includes(filtro.toLowerCase())) }
+
+            )
+        }
+
+    }, [PoliticiState, filtro])
+
 
 
 
     return (
-        <div className='grid sm:grid-cols-3 m-2  md:grid-cols-2 lg:grid-cols-4 gap-2 '>
-            {
-                PoliticiState.map(politico => {
-                    return (
-                        <div key={politico.id}>
-                            <Card politico={politico} />
-                        </div>
-                    )
-                })
-            }
-        </div>
+        <>
+            <input type="text" id="politico" value={filtro} onChange={(e) => handleInput(e)} className='bg-red-500' />
+            <div className='grid sm:grid-cols-3 m-2  md:grid-cols-2 lg:grid-cols-4 gap-2 '>
+                {
+                    filtredArray.map(politico => {
+                        return (
+                            <div key={politico.id}>
+                                <Card politico={politico} />
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        </>
+
     )
 }
 
