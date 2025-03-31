f5af485462e0ca6e8fc39602cc0d4388a5a69ce3
Orsouene <orsoueneelaouizeb@gmail.com>
2025-03-31 13:49:09 +0200

    Milestone 3

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,17 +1,21 @@
-import React from 'react'
 
-function Card({ politico }) {
+import React, { memo } from 'react'
+
+const Card = memo(({ politico }) => {
+    console.log("Card")
     return (
-        <div className='bg-stone-200 mb-3 text-stone-700 rounded-xl max-w-64 p-2 m-auto h-56'>
-            <h3><span className='text-xl text-black'>Nome :</span>{politico.name}</h3>
-            {/* <div> <span className='text-xl text-black'>Immagine :</span>
-                <img src="http://someImage.png" alt="" />
-            </div> */}
+        <div className='bg-stone-200 mb-3 text-stone-700 rounded-xl max-w-64 p-2 m-auto h-96 border-1 hover:border-amber-500'>
+            <h3 ><span className='text-xl text-black' >Nome :</span>{politico.name}</h3>
+            <div className='flex gap-16'> <span className='text-xl text-black'>Immagine :</span>
+                <img src={politico.image} className='h-20' alt="" />
+            </div>
 
-            <p> <span className='text-xl text-black'>Posizione :</span>{politico.occupation}</p>
-            <p > <span className='text-xl text-black'>Breve biografia  :</span>my name is {politico.name} and my hobbies are {politico.hobbies[0]} and {politico.hobbies[1]} </p>
+            <p> <span className='text-xl text-black'>Posizione :</span>{politico.position}</p>
+            <p > <span className='text-xl text-black'> Breve biografia  : </span> {politico.biography} </p>
         </div>
     )
-}
+
+})
+
 
 export default Card
--- src/Components/Politici.jsx
+++ src/Components/Politici.jsx
@@ -3,47 +3,45 @@ import Card from './Card';
 function Politici() {
     const [PoliticiState, setPoliticiState] = useState([])
     const [filtro, setFiltro] = useState("")
-    const dataFetch = () => {
 
-        fetch("https://boolean-spec-frontend.vercel.app/freetestapi/users").then(res => res.json()).then(data => {
+    useEffect(() => {
+        fetch("https://boolean-spec-frontend.vercel.app/freetestapi/politicians").then(res => res.json()).then(data => {
             setPoliticiState(data)
-            console.log(data)
+
         }
         ).catch(err => console.log(err));
-
-    }
-    useEffect(() => {
-        dataFetch();
     }, []);
 
     function handleInput(e) {
         const value = e.target.value
         setFiltro(value)
     }
-
+    // console.log(PoliticiState)
     const filtredArray = useMemo(() => {
         if (!filtro) {
-            return PoliticiState
-        }
-        else {
-            return PoliticiState.filter(politico => { return politico.name.toLowerCase().includes(filtro.toLowerCase()) || politico.hobbies.find((el) => el.toLowerCase().includes(filtro.toLowerCase())) }
-
-            )
+            return PoliticiState;
         }
 
-    }, [PoliticiState, filtro])
+        return PoliticiState.filter(politico =>
+            politico.name.toLowerCase().includes(filtro.toLowerCase()) ||
+            politico.biography.toLowerCase().includes(filtro.toLowerCase())
+        );
+    }, [PoliticiState, filtro]); // solo filtro è una dipendenza
+
 
 
 
 
     return (
-        <>
-            <input type="text" id="politico" value={filtro} onChange={(e) => handleInput(e)} className='bg-red-500' />
+        <>  <div className='flex justify-center p-2' >
+            <input type="text" id="politico" placeholder="Cerca" value={filtro} onChange={(e) => handleInput(e)} className='bg-stone-500  text-white border  hover:border-amber-500 p-2' />
+        </div>
+
             <div className='grid sm:grid-cols-3 m-2  md:grid-cols-2 lg:grid-cols-4 gap-2 '>
                 {
                     filtredArray.map(politico => {
                         return (
-                            <div key={politico.id}>
+                            <div key={politico.id} >
                                 <Card politico={politico} />
                             </div>
                         )
