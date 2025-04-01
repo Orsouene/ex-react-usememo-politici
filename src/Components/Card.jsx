7a4e1ce9558480cdffa14d41395959c48ddb2695
Orsouene <orsoueneelaouizeb@gmail.com>
2025-04-01 14:20:49 +0200

    Bonus

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,9 +2,9 @@
 import React, { memo } from 'react'
 
 const Card = memo(({ politico }) => {
-    console.log("Card")
+    // console.log("Card")
     return (
-        <div className='bg-stone-200 mb-3 text-stone-700 rounded-xl max-w-64 p-2 m-auto h-96 border-1 hover:border-amber-500'>
+        <div className='bg-stone-200 mb-3 text-stone-700 rounded-xl max-w-64 p-2 m-auto h-full border-1 hover:border-amber-500'>
             <h3 ><span className='text-xl text-black' >Nome :</span>{politico.name}</h3>
             <div className='flex gap-16'> <span className='text-xl text-black'>Immagine :</span>
                 <img src={politico.image} className='h-20' alt="" />
--- src/Components/Politici.jsx
+++ src/Components/Politici.jsx
@@ -3,37 +3,61 @@ import Card from './Card';
 function Politici() {
     const [PoliticiState, setPoliticiState] = useState([])
     const [filtro, setFiltro] = useState("")
-
+    const [filtroPosition, setFiltroPosition] = useState("")
+    //*fetch degli politici 
     useEffect(() => {
+        console.log(PoliticiState);
         fetch("https://boolean-spec-frontend.vercel.app/freetestapi/politicians").then(res => res.json()).then(data => {
             setPoliticiState(data)
-
+            console.log(PoliticiState)
         }
         ).catch(err => console.log(err));
     }, []);
 
+    //*HandleInput
     function handleInput(e) {
         const value = e.target.value
         setFiltro(value)
     }
-    // console.log(PoliticiState)
+    //* filtrare larray di Politici dipende la loro positions
+    const filtredPosition = useMemo(() => {
+        const arrayPosizioneFiltrato = []
+        // console.log(PoliticiState)
+        PoliticiState.forEach(el => {
+            if (!arrayPosizioneFiltrato.includes(el.position)) {
+                arrayPosizioneFiltrato.push(el.position)
+            }
+
+        })
+
+        return arrayPosizioneFiltrato
+    }, [PoliticiState])
+    console.log(filtroPosition)
+
+    //*Filtrare l'array degli politici per ottnere la ricerca voluta 
     const filtredArray = useMemo(() => {
-        if (!filtro) {
-            return PoliticiState;
-        }
 
-        return PoliticiState.filter(politico =>
-            politico.name.toLowerCase().includes(filtro.toLowerCase()) ||
-            politico.biography.toLowerCase().includes(filtro.toLowerCase())
-        );
-    }, [PoliticiState, filtro]); // solo filtro è una dipendenza
+        return PoliticiState.filter(politico => {
+            return (politico.name.toLowerCase().includes(filtro.toLowerCase()) ||
+                politico.biography.toLowerCase().includes(filtro.toLowerCase())) &&
+                filtroPosition.toLowerCase() === politico.position.toLowerCase()
 
-
-
-
-
+        })
+    }, [PoliticiState, filtro, filtroPosition]);
     return (
-        <>  <div className='flex justify-center p-2' >
+        <>  <div className='flex justify-center p-2 gap-5' >
+            <select
+                className='text-white w-48 '
+                value={filtroPosition}
+                onChange={e => setFiltroPosition(e.target.value)}>
+                <option value="" className='text-black'>Select position</option>
+                {
+                    filtredPosition.map((el, index) => (
+                        <option key={index} value={el} className='text-black ' >{el}</option>
+                    ))
+                }
+
+            </select>
             <input type="text" id="politico" placeholder="Cerca" value={filtro} onChange={(e) => handleInput(e)} className='bg-stone-500  text-white border  hover:border-amber-500 p-2' />
         </div>
 
